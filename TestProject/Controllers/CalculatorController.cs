0097387a2c9ebe23550bc407d9108508a973e4f6
azmirizda <azmirizda@gmail.com>
2022-02-04 20:34:22 +0700

    push project

---
+++ TestProject/Controllers/CalculatorController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestProject.Models;
+
+namespace TestProject.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            Calculator Model = new Calculator();
+            return View(Model);
+        }
+
+        [HttpPost]
+        public IActionResult Add(Calculator model)
+        {
+            model.Result = model.FirstNumber + model.SecondNumber;
+            return View("Index",model);
+        }
+
+        [HttpPost]
+        public IActionResult Substract(Calculator model)
+        {
+            model.Result = model.FirstNumber - model.SecondNumber;
+            return View("Index", model);
+        }
+
+        [HttpPost]
+        public IActionResult Divide(Calculator model)
+        {
+            model.Result = model.FirstNumber / model.SecondNumber;
+            return View("Index", model);
+        }
+
+        [HttpPost]
+        public IActionResult Multiply(Calculator model)
+        {
+            model.Result = model.FirstNumber * model.SecondNumber;
+            return View("Index", model);
+        }
+    }
+}
---
+++ TestProject/Models/Calculator.cs
@@ -0,0 +1,9 @@
+﻿namespace TestProject.Models
+{
+    public class Calculator
+    {
+        public decimal FirstNumber { get; set; }
+        public decimal SecondNumber { get; set; }
+        public decimal Result { get; set; }
+    }
+}
